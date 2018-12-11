05bd8011ca6f669090b6281184c8b2fb0a43fd35
guolongganga <15501261520@163.com>
2018-12-11 11:05:16 +0800

    提交分支

--- app/src/main/java/com/example/hp/menudemo100/MainActivity.java
+++ app/src/main/java/com/example/hp/menudemo100/MainActivity.java
@@ -2,9 +2,10 @@ package com.example.hp.menudemo100;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+   private ListView lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
